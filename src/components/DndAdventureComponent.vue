a5bc2449123c15a02d46b9f260503914302e4004
niveau-superieur <niveausuperieurns@gmail.com>
2026-04-20 21:58:08 +0200

    feat: ajout des images des aventures

--- src/components/DndAdventureComponent.vue
+++ src/components/DndAdventureComponent.vue
@@ -9,16 +9,18 @@ defineProps<{ dndAdventure: DndAdventure; reverse?: boolean }>()
     :href="dndAdventure.notionUrl"
     target="_blank"
     :class="[
-      'flex flex-col md:flex-row gap-4 md:gap-6 rounded-xl p-4 md:p-6 transition hover:scale-',
+      'flex flex-col md:flex-row items-center gap-4 md:gap-6 rounded-xl p-4 md:p-6 transition md:hover:scale-105',
       reverse ? 'md:flex-row-reverse card-reverse' : 'card',
     ]"
   >
     <div class="w-full md:w-1/2">
-      <img
-        class="h-48 md:h-full w-full rounded-md object-cover"
-        :src="dndAdventure.image"
-        :alt="dndAdventure.title"
-      />
+      <div class="aspect-video max-h-64 w-full flex overflow-hidden">
+        <img
+          class="h-full w-full object-contain rounded-xl"
+          :src="dndAdventure.image"
+          :alt="dndAdventure.title"
+        />
+      </div>
     </div>
 
     <div class="flex-1 flex-col">
