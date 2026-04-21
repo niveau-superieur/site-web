8e871106145516f2e8319f8bb314c622f3156c61
niveau-superieur <niveausuperieurns@gmail.com>
2026-04-21 18:39:18 +0200

    fix: suppression du vue-devtools inutilisé

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), tailwindcss()],
   base: '/site-web/',
   resolve: {
     alias: {
